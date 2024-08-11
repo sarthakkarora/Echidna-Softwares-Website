13c17a08ebf4f484a40a72b5f9f82eaf412688e9
Sarthak Arora <sarthakk.arora1@gmail.com>
2024-08-12 03:57:18 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,136 @@
+(function($) {
+  "use strict";
+
+  /*--------------------------
+  preloader
+  ---------------------------- */
+  $(window).on('load', function() {
+    var pre_loader = $('#preloader');
+    pre_loader.fadeOut('slow', function() {
+      $(this).remove();
+    });
+  });
+
+  /*---------------------
+   TOP Menu Stick
+  --------------------- */
+  /*var s = $("#sticker");
+  var pos = s.position();
+  $(window).on('scroll', function() {
+    var windowpos = $(window).scrollTop() > 300;
+    if (windowpos > pos.top) {
+      s.addClass("stick");
+    } else {
+      s.removeClass("stick");
+    }
+  });*/
+
+  /*----------------------------
+   Navbar nav
+  ------------------------------ */
+  /*var main_menu = $(".main-menu ul.navbar-nav li ");
+  main_menu.on('click', function() {
+    main_menu.removeClass("active");
+    $(this).addClass("active");
+  });*/
+
+  /*----------------------------
+   wow js active
+  ------------------------------ */
+  new WOW().init();
+
+  $(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+
+  //---------------------------------------------
+  //Nivo slider
+  //---------------------------------------------
+  $('#ensign-nivoslider').nivoSlider({
+    effect: 'random',
+    slices: 15,
+    boxCols: 12,
+    boxRows: 8,
+    animSpeed: 500,
+    pauseTime: 5000,
+    startSlide: 0,
+    directionNav: true,
+    controlNavThumbs: false,
+    pauseOnHover: true,
+    manualAdvance: false,
+  });
+
+  /*----------------------------
+   Scrollspy js
+  ------------------------------ */
+  window.bootstrap = bootstrap;
+  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '.navbar-collapse',
+    offset: 80
+  })
+
+  /*----------------------------
+  Page Scroll
+  ------------------------------ */
+  /*var page_scroll = $('a.page-scroll');
+	console.log("page_scroll")
+	console.log(page_scroll)
+  page_scroll.on('click', function(event) {
+	console.log("indexxxx")
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 55
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });*/
+
+  /*--------------------------
+    Back to top button
+  ---------------------------- */
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+  /*--------------------------
+   collapse
+  ---------------------------- */
+  var panel_test = $('.panel-heading a');
+  panel_test.on('click', function() {
+    panel_test.removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*---------------------
+     Google Maps
+  --------------------- */
+  // Fr use with dynamic Google maps
+  if( $('#google-map').length) {
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+      var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+      var mapOptions = {
+        zoom: 14,
+        scrollwheel: false,
+        center: myLatlng
+      };
+      var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map
+      });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+  }
+
+})(jQuery);
